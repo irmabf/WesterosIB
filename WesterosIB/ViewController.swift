c7b6ff5caedb87306ccb784e53211f72a35341a6
Irma <irblancofdz@gmail.com>
2018-02-26 18:21:13 +0100

    Initial Commit

---
+++ WesterosIB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WesterosIB
+//
+//  Created by Irma Blanco on 26/02/2018.
+//  Copyright © 2018 Irma Blanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
